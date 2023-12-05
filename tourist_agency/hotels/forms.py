47ac4eff0c88bb32b40b463c2338958f845b6006
nesonzahartms <nesonzahar@gmail.com>
2023-12-05 21:49:43 +0300

    xxzv

---
+++ tourist_agency/hotels/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Review, AgencyUser, Booking
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Форма поиска отелей
+class HotelSearchForm(forms.Form):
+    name = forms.CharField(required=False)
+    country = forms.CharField(required=False)
+    category = forms.IntegerField(required=False)
+    options = forms.CharField(required=False)
+
+
+# Форма создания отзыва
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['rating', 'comment']
+
+
+# Форма для входа
+class AgencyLoginForm(AuthenticationForm):
+    pass
+
+
+# Форма для регистрации
+class AgencyRegistrationForm(UserCreationForm):
+    class Meta:
+        model = AgencyUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['check_in', 'check_out']
---
+++ tourist_agency/hotels/urls.py
@@ -0,0 +1,15 @@
+from . import views
+from django.urls import path
+from .views import hotel_search, create_review, hotel_gallery, agency_login, agency_registration, homepage
+
+urlpatterns = [
+    # path('hotels/', views.HotelListView.as_view(), name='hotel_list'),
+    # path('hotels/<int:pk>/', views.HotelDetailView.as_view(), name='hotel_detail'),
+    path('hotels/search/', hotel_search, name='hotel_search'),
+    path('hotels/<int:hotel_id>/gallery/', hotel_gallery, name='hotel_gallery'),
+    path('agencies/<int:agency_id>/reviews/create/', create_review, name='create_review'),
+    path('login/', agency_login, name='agency_login'),
+    path('register/', agency_registration, name='agency_registration'),
+    path('', homepage, name='homepage'),
+
+]
